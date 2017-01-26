c363eb05af67c02ef4997ffdbd08368f55ad0ae0
orangevtr <htakayu1975@gmail.com>
2017-01-26 18:26:52 +0900

    add example to handle exception

--- test/test-divided.js
+++ test/test-divided.js
@@ -6,9 +6,23 @@ describe('divided', function() {
     describe('#calculate', function() {
         it('should return 2 when the value is 4', function() {
             divided.calculate(4).should.equal(2);
+//            expect(divided.calculate(4)).to.equal(2);
         });
         it('should return 1 when the value is 3', function() {
             divided.calculate(3).should.equal(1);
+        });
+        it('should throw exceptions when the value are other than numbers', function() {
+            (divided.calculate).should.throw(Error);
+            (function() {divided.calculate(null)}).should.throw(Error, 'Type of numeric is expected.');
+            (function() {divided.calculate("abc")}).should.throw(Error, / numeric /);
+            (function() {divided.calculate([])}).should.throw(Error, /^Type of numeric /);
+
+            //// expect-style sentences:
+            // var expect = chai.expect;
+            // expect(divided.calculate).to.throw(Error);
+            // expect(function() {divided.calculate(null)}).to.throw(Error, 'Type of numeric is expected.');
+            // expect(function() {divided.calculate("abc")}).to.throw(Error, / numeric /);
+            // expect(function() {divided.calculate([])}).to.throw(Error, /^Type of numeric /);
         });
     });
 });
